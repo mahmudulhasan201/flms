0ab8a3fe4da78c53dd491635403c95d1c22d05fe
Mahmud Hasan <mahmud00hasan09@gmail.com>
2024-05-16 13:13:19 +0600

    replace login form

---
+++ resources/views/frontend/pages/team/teamLogin.blade.php
@@ -0,0 +1,34 @@
+@extends('frontend.master')
+@section('content')
+
+<div class="content-inner">
+<div class="row" style="margin-top: 20px; margin-bottom:20px;">
+
+<div class="col-md-3">
+</div>
+
+<div class="col-md-6" style="border:2px solid black; padding:10px; border-radius:15px;">
+
+<form action="{{route('team.login')}}" method="post">
+    @csrf
+  <div class="form-group">
+    <label for="">Email address</label>
+    <input name="email_address" type="email" class="form-control" id=""  placeholder="Enter email">
+
+  </div>
+  <div class="form-group">
+    <label for="">Password</label>
+    <input name="password" type="password" class="form-control" id="" placeholder="Password">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+
+<div class="col-md-3">
+</div>
+
+</div>
+</div>
+
+@endsection
