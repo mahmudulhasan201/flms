a67b40a5ce2311645f6e14776fee7afc244140c6
Riderr470 <riderr40@gmail.com>
2024-05-26 13:49:37 +0600

    added few reports in dashboard

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -1,12 +1,64 @@
-
 @extends('backend.master')
 
 @section('content')
-<div style="text-align: center; padding: 300px">
-    <h1 style="font-weight: bold; font-size: 80px; margin-bottom: 20px;">Hello Admin</h1>
-    
-    <!-- <div style="text-align: center;">
-        <img src="" alt="Project Image" style="width: 600px; height: 400px; display: block; margin: 0 auto;">
-    </div>     -->
+<div class="row mt-3">
+    <div class="col-lg-6 col-xl-3 mb-4">
+        <div class="card bg-primary text-white h-100">
+            <div class="card-body">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="me-3">
+                        <div class="text-white-75 small">Total Players</div>
+                        <div class="text-lg fw-bold">{{$players}}</div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="card-footer d-flex align-items-center justify-content-between small">
+                <a class="text-white stretched-link" href="{{route('player.list')}}">View Details</a>
+                <div class="text-white"><svg class="svg-inline--fa fa-angle-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg="">
+                        <path fill="currentColor" d="M246.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 41.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path>
+                    </svg></div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-xl-3 mb-4">
+        <div class="card bg-warning text-white h-100">
+            <div class="card-body">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="me-3">
+                        <div class="text-white-75 small">Total Teams</div>
+                        <div class="text-lg fw-bold"> {{$teams}} </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="card-footer d-flex align-items-center justify-content-between small">
+                <a class="text-white stretched-link" href="{{route('team.list')}}">View Details</a>
+                <div class="text-white"><svg class="svg-inline--fa fa-angle-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg="">
+                        <path fill="currentColor" d="M246.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 41.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path>
+                    </svg><!-- <i class="fas fa-angle-right"></i> Font Awesome fontawesome.com --></div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-xl-3 mb-4">
+        <div class="card bg-success text-white h-100">
+            <div class="card-body">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="me-3">
+                        <div class="text-white-75 small">Total Leagues</div>
+                        <div class="text-lg fw-bold">{{$leagues}}</div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="card-footer d-flex align-items-center justify-content-between small">
+                <a class="text-white stretched-link" href="{{route('league.list')}}">View Details</a>
+                <div class="text-white"><svg class="svg-inline--fa fa-angle-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg="">
+                        <path fill="currentColor" d="M246.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 41.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path>
+                    </svg><!-- <i class="fas fa-angle-right"></i> Font Awesome fontawesome.com --></div>
+            </div>
+        </div>
+    </div>
+
 </div>
 @endsection
